760985db1c3e1deaf401859354e23cf6217bbdf7
Ashiamd <ashiamd@foxmail.com>
2021-05-29 16:08:57 +0800

    1. 单例模式-枚举 => 代码实现和测试

---
+++ src/main/java/ash/singleton/Singleton004.java
@@ -0,0 +1,79 @@
+package ash.singleton;
+
+/**
+ * 枚举
+ * 优点：
+ *  1. 线程安全
+ *  2. 本身防止反射破解单例实现
+ *  3. 本身防止序列化破解单例实现
+ * 缺点：
+ *  1. 使用场景少
+ * @author : Ashiamd email: ashiamd@foxmail.com
+ * @date : 2021/5/29 3:52 PM
+ */
+public enum Singleton004 {
+    INSTANCE{
+        public final String var1 = "HELLO";
+        public final String var2 = "WORLD";
+        public final Integer var3 = 129;
+    }
+}
+
+// 1. 测试 单例类实现
+/*class Test01 {
+    public static void main(String[] args) {
+        // 'Singleton004()' has private access in 'ash.singleton.Singleton004'
+        // Singleton004 instance = new Singleton004();
+        Singleton004 instance01 = Singleton004.INSTANCE;
+        Singleton004 instance02 = Singleton004.INSTANCE;
+        System.out.println(instance01==instance02); // true
+    }
+}*/
+
+// 2. 测试 是否能够通过反射破解单例
+/**
+ * Exception in thread "main" java.lang.NoSuchMethodException: ash.singleton.Singleton004.<init>()
+ * 	at java.lang.Class.getConstructor0(Class.java:3082)
+ * 	at java.lang.Class.getDeclaredConstructor(Class.java:2178)
+ * 	at ash.singleton.Test02.main(Singleton004.java:39)
+ */
+/*class Test02 {
+    public static void main(String[] args) throws Exception{
+        Class clazz = Class.forName("ash.singleton.Singleton004");
+        Constructor constructor = clazz.getDeclaredConstructor();
+
+        constructor.setAccessible(true);
+
+        Singleton004 instance01 = Singleton004.INSTANCE;
+        Singleton004 instance02 = Singleton004.INSTANCE;
+        Singleton004 instance03 = (Singleton004) constructor.newInstance();
+        Singleton004 instance04 = (Singleton004) constructor.newInstance();
+
+        System.out.println(instance01 == instance02); // true
+        System.out.println(instance02 == instance03); // false
+        System.out.println(instance03 == instance04); // false
+    }
+}*/
+
+// 3. 测试 是否能够通过序列化/反序列化破解单例
+/*class Test03 {
+    public static void main(String[] args) throws Exception {
+        Singleton004 instance001 = Singleton004.INSTANCE;
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+                new FileOutputStream("Singleton.txt")
+        );
+        objectOutputStream.writeObject(instance001);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(
+                new FileInputStream("Singleton.txt")
+        );
+        Singleton004 instance002 = (Singleton004) objectInputStream.readObject();
+
+        System.out.println(instance001); // INSTANCE
+        System.out.println(instance002); // INSTANCE
+        System.out.println(instance001 == instance002); // true
+    }
+}*/
