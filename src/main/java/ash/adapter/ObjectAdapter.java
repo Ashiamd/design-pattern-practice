b90d8ce46612579d91e6f1fe5bdb90d6d7959ef4
Ashiamd <ashiamd@foxmail.com>
2021-05-30 21:21:28 +0800

    6. 适配器模式-对象适配器 => 代码实现和测试

--- src/main/java/ash/adapter/ObjectAdapter.java
+++ src/main/java/ash/adapter/ObjectAdapter.java
@@ -1,19 +1,20 @@
 package ash.adapter;
 
 /**
- * 类适配器
- * 继承被适配的类，实现目标接口
+ * 对象适配器
+ * 依赖(聚合/组合)被适配的类，实现目标接口
  * @author : Ashiamd email: ashiamd@foxmail.com
- * @date : 2021/5/30 8:13 PM
+ * @date : 2021/5/30 9:11 PM
  */
-public class ClassAdapter {
+public class ObjectAdapter {
     public static void main(String[] args) {
         Phone phone = new Phone();
-        phone.charge(new VoltageAdapter());
+        VoltageAdapter voltageAdapter = new VoltageAdapter();
+        voltageAdapter.voltage220VAdapter(new Voltage220V());
+        phone.charge(voltageAdapter);
     }
 }
 
-/*
 interface Voltage5V{
     void output5V();
 }
@@ -31,11 +32,18 @@ class Voltage220V{
     }
 }
 
-class VoltageAdapter extends Voltage220V implements Voltage5V{
+class VoltageAdapter implements Voltage5V{
+
+    Voltage220V voltage220V;
+
+    void voltage220VAdapter(Voltage220V voltage220V){
+        this.voltage220V = voltage220V;
+    }
+
     @Override
     public void output5V() {
-        output220V();
+        voltage220V.output220V();
         System.out.println("adapter ~ 220v => 5v");
         System.out.println("output ~ 5v");
     }
-}*/
+}
